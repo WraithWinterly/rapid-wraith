7a64fc2e2709f13d2e521f9882a48f6f9cc32947
WraithWinterly <37941646+WraithWinterly@users.noreply.github.com>
2022-09-09 23:29:46 -0400

    Information Section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./components/**/*.{html,js}"],
+  purge: ["./src/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
     extend: {},
+    mode: "jit",
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 };
