5cb70ff90a28eb6443fe5c97f6f64b8f6861e1ad
WraithWinterly <37941646+WraithWinterly@users.noreply.github.com>
2022-09-11 20:49:13 -0400

    Carousel and Menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import "./input.css";
+
+const viewMenuButton = document.getElementById("menu-collaspe-button");
+
+viewMenuButton.addEventListener("click", () => {
+  const expanded = viewMenuButton.getAttribute("aria-expanded") === "true";
+  viewMenuButton.innerHTML = expanded ? "Hide Menu" : "View Menu";
+});
